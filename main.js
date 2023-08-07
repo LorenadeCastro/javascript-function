ff6fd489e1de3314d71cd2d02d858bc20593b25a
Lorena de Castro <139800465+LorenadeCastro@users.noreply.github.com>
2023-08-07 16:15:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function divisao(a,b) {
+    if (a / b === 50) {
+        console.log((a / b) + " e igual a 50.");
+
+
+    } else {
+        console.log((a / b) + " nao e igual a 50.");
+    }
+    }
+
+
+    console.log(divisao(50,2))
